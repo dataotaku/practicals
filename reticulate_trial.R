43eb1c4bc7f680d02aa93cd5dbcefe9b329893b6
unho.chang <capcock@snu.ac.kr>
2023-08-10 12:27:55 +0900

    rmd 파일 포함

---
+++ reticulate_trial.R
@@ -0,0 +1,24 @@
+# install.packages("tinytex")
+# install.packages("formatR")
+# 
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install(c("graph", "Rgraphviz"), dep=TRUE)
+# 
+# install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/stable"), dep=TRUE)
+# 
+# install.packages("INLA",repos=c(getOption("repos"),INLA="https://inla.r-inla-download.org/R/testing"), dep=TRUE)
+# 
+# install.packages("BAS")
+# 
+# install.packages("readr")
+# install.packages("ggfortify")
+# 
+# install.packages("reticulate")
+
+library(reticulate)
+path_to_python <- install_python()
+virtualenv_create("r-reticulate", python = path_to_python)
+
+virtualenv_install("r-reticulate", "scipy")
+virtualenv_install("r-reticulate", "seaborn")
